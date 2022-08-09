b7a57ab18f43db55f0300e7743b1f28ab8d4bfd1
DrymemoG-CVO <104378190+DrymemoG-CVO@users.noreply.github.com>
2022-08-09 11:51:54 -0600

    Se agregó el Load

--- ProyectoTienda/ProyectoTienda/Form1.vb
+++ ProyectoTienda/ProyectoTienda/Form1.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form1
   Dim variable as String
-  Dim variable2 as integer
+  Dim variable2 as Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Hola Mundo")
+    End Sub
 End Class
